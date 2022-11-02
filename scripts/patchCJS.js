8f8c0e4394518cd3384083d23deaf7ea890080de
yujinpan <1192878390@qq.com>
2022-11-02 15:30:50 +0800

    chore: patch with all modules

--- scripts/patchCJS.js
+++ scripts/patchCJS.js
@@ -1,12 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-const indexFile = path.resolve(__dirname, '../lib/index.js');
-const code = fs.readFileSync(indexFile).toString();
+[
+  path.resolve(__dirname, '../lib/index.js'),
+  path.resolve(__dirname, '../lib/zip.js'),
+].forEach(patchCJS);
 
-fs.writeFileSync(
-  indexFile,
-  code
-    .replace(/\nexports\.default(.*)\n/, '\nexports$1\nexports["default"]$1\n')
-    .replace(/\nexports(\W)/g, '\nmodule.exports$1'),
-);
+function patchCJS(filepath) {
+  const code = fs.readFileSync(filepath).toString();
+
+  fs.writeFileSync(
+    filepath,
+    code
+      .replace(
+        /\nexports\.default(.*)\n/,
+        '\nexports$1\nexports["default"]$1\n',
+      )
+      .replace(/\nexports(\W)/g, '\nmodule.exports$1'),
+  );
+}
